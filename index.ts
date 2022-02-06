4e901141dedc4bdf3cfadf11fe7b836e32849894
Max Hogan <me@maxhogan.dev>
2022-02-06 16:58:05 -0600

    Add slash commands

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import {Collection} from "@discordjs/collection";
-import {Snowflake} from "discord-api-types";
-import {OAuth2Guild, Client, Intents, TextChannel, MessageOptions, Guild} from "discord.js";
+import {Routes, Snowflake} from "discord-api-types/v9";
+import {OAuth2Guild, Client, Intents, TextChannel, MessageOptions, Guild, Permissions} from "discord.js";
 import {Timestamp, Firestore} from '@google-cloud/firestore';
 import {readFileSync} from "fs";
+const { REST } = require('@discordjs/rest');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MEMBERS] });
 
@@ -11,15 +12,15 @@ interface guildDoc{
     channelId: Snowflake
 }
 
-const db = new Firestore({keyFilename: "serviceAccount.json"});
-
 const reactions = ["🔴", "🟠", "🟡", "🟢", "👍"];
 const checkInterval = 1000 * 60 * 30;
 
-const runHealthCheckForGuild = async (guild : OAuth2Guild) => {
+const db = new Firestore({keyFilename: "serviceAccount.json"});
+const token = readFileSync("token.txt").toString().trim();
+
+const runHealthCheckForGuild = async (guild : OAuth2Guild | Guild, overrideDailyCheck?: boolean) => {
     const docRef = db.collection("guilds").doc(guild.id);
     const document = await docRef.get();
-
 
     console.log(`Found doc for ${guild.id}: ${document.exists}`);
 
@@ -34,7 +35,7 @@ const runHealthCheckForGuild = async (guild : OAuth2Guild) => {
 
         const didNotCheckInToday = !date || date.toDateString() !== (new Date()).toDateString();
 
-        if(didNotCheckInToday && lateEnoughInDay){
+        if((didNotCheckInToday && lateEnoughInDay) || overrideDailyCheck){
             console.log("Running checkin");
             const thisGuild = client.guilds.cache.get(guild.id);
 
@@ -44,7 +45,7 @@ const runHealthCheckForGuild = async (guild : OAuth2Guild) => {
             const channel = await thisGuild.channels.fetch(data.channelId) as TextChannel;
 
             const message:MessageOptions = {
-                content: `@everyone Please complete your daily health checkin!\nReact with your current status`,
+                content: `@everyone Please complete your daily health checkin!\n\n(React with your current status)`,
                 files: ["https://cdn.discordapp.com/attachments/878399508277497926/935192353478680586/mental-health-continuum-model.png"]
             };
 
@@ -86,6 +87,43 @@ client.on("ready", async() => {
     setInterval(checkForAllGuilds, checkInterval);
 });
 
+client.on("interactionCreate", async interaction => {
+    if (!interaction.isCommand()) return;
+
+    if (interaction.commandName === "checkin") {
+        if(interaction.guild) {
+            await interaction.deferReply({ephemeral: true});
+
+            if(interaction.memberPermissions && interaction.memberPermissions.has(Permissions.FLAGS.MENTION_EVERYONE)) {
+                await runHealthCheckForGuild(interaction.guild, true);
+            }
+            await interaction.editReply({content: "👍"});
+        }else{
+            await interaction.reply({ephemeral: true, content: "Please run this command in a server text channel."});
+        }
+    }
+});
+
 
 // noinspection JSIgnoredPromiseFromCall
-client.login(readFileSync("token.txt").toString().trim());
+client.login(token);
+
+const commands = [{
+    name: "checkin",
+    description: "(Manually) runs the daily health checkin for this server"
+}];
+
+const rest = new REST({ version: "9" }).setToken(token);
+
+(async () => {
+    try {
+        await rest.put(
+            Routes.applicationGuildCommands("939986851593355294", "340940153424052225"),
+            { body: commands },
+        );
+
+        console.log("Successfully reloaded slash commands.");
+    } catch (error) {
+        console.error(error);
+    }
+})();
